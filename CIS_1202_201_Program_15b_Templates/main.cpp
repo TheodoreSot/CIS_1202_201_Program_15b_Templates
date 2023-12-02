f8bd5c4700f2b76822ce840b56d14ff5b1e9f9ca
Theodore Sotirelis <tg.sotirelis@gmail.com>
2023-12-01 22:09:07 -0500

    Initial Commit

---
+++ CIS_1202_201_Program_15b_Templates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS_1202_201_Program_15b_Templates
+//
+//  Created by Theodore Gabriel Sotirelis on 12/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

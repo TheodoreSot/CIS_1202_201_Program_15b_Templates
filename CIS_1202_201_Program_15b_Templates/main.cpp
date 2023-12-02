51837a54bd09f2df6ff799ea5df148645fbb9c68
Theodore Sotirelis <tg.sotirelis@gmail.com>
2023-12-01 22:33:38 -0500

    Added templated function, and regular function overloading templated function.

--- CIS_1202_201_Program_15b_Templates/main.cpp
+++ CIS_1202_201_Program_15b_Templates/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  CIS_1202_201_Program_15b_Templates
-//
-//  Created by Theodore Gabriel Sotirelis on 12/1/23.
-//
-
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template <typename T>
+T half(T num)
+{
+    T halfNum=(num/2);
+    return halfNum;
+}
+
+//template <typename T>
+int half(int num)
+{
+    int roundedNum;
+    double tempNum=static_cast<double>(num)/2;
+    roundedNum=round(static_cast<double>(tempNum));
+    return roundedNum;
+    
+}
+
+int main()
+{
+    double a=7.0;
+    float b=5.0f;
+    int c=3;
+    
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+
     return 0;
 }
